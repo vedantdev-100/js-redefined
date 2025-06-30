e24b864662013e7dda89f224fa2e58e3ba933452
vedantdev-100 <ivedant.100@gmail.com>
2025-06-30 20:15:07 +0000

    datatypes added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "ivedant.11@gmail.com"
+var accountPassword = "3eijklf34"
+accountCity = "Nagpur"
+let accountState
+
+accountEmail = "ivedant.1001@gmail.com"
+accountPassword = "3eijklfasdf34"
+accountCity = "Pune"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict" // treat all JS code as newer version
+
+// alert("hello") // we are using node.js not browser
+
+
+// number => 2^53
+// bigint
+// string => ""
+// boolean 
+// null => standalone value (empty)
+// undefined 
+// symbol => unique
+
+
+//object 
