dc0b7accdcdfcbac882da235da5648dfcf62f897
vedantdev-100 <ivedant.100@gmail.com>
2025-06-30 20:28:42 +0000

    conversionDatatypes added

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score = "22"
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// "22" => 22 
+// "22asd" => NaN
+// true => 1 false => 0
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// "" => false, "adlkfj" => true
